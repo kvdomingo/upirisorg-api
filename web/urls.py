6135f1c7c0215670ec75f1220bf055c7962009ec
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-03-16 01:28:22 +0800

    draft homepage

--- web/urls.py
+++ web/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name='index'),
+    path('', views.home, name='home'),
+    path('about', views.construction, name='about'),
+    path('members', views.construction, name='members'),
+    path('alumni', views.construction, name='alumni'),
+    path('gallery', views.construction, name='gallery'),
+    path('contact', views.construction, name='contact'),
 ]
--- web/views.py
+++ web/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def home(request):
     context = {
         'active_page': 'home',
     }
     return render(request, 'web/index.html.j2', context)
+
+def construction(request):
+    return render(request, 'web/construction.html.j2')
