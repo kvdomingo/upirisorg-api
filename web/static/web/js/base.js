4c9b92964bfec134d0050a38b82f435b197d689e
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-04-25 14:24:49 +0800

    update header photos

--- web/static/web/js/base.js
+++ web/static/web/js/base.js
@@ -7,16 +7,16 @@ var cl = new cloudinary.Cloudinary({
 const img_dir = "upirisorg/web/static/web/media/private";
 
 document.addEventListener("DOMContentLoaded", () => {
-    $("body").removeClass("fade-out");
-    $("a").each(function() {
-        $(this).on('click', function() {
-            is_href = $(this)[0].hasAttribute("href");
-            valid_href = $(this).first().attr("href") !== "#";
-            is_target = $(this)[0].hasAttribute("target");
-            targets_in = $(this).first().attr("target") !== "_blank";
-            if ((is_href && valid_href) || (is_target && targets_in)) {
-                $('body').addClass('fade-out');
-            }
-        });
-    });
+    // $("body").removeClass("fade-out");
+    // $("a").each(function() {
+    //     $(this).on('click', function() {
+    //         is_href = $(this)[0].hasAttribute("href");
+    //         valid_href = $(this).first().attr("href") !== "#";
+    //         is_target = $(this)[0].hasAttribute("target");
+    //         targets_in = $(this).first().attr("target") !== "_blank";
+    //         if ((is_href && valid_href) || (is_target && targets_in)) {
+    //             $('body').addClass('fade-out');
+    //         }
+    //     });
+    // });
 });
--- web/static/web/js/index.js
+++ web/static/web/js/index.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
         ],
     });
 
-    img_url = "https://scontent.fcrk1-1.fna.fbcdn.net/v/t1.0-9/60627711_2460297753981013_8365258745302745088_n.jpg?_nc_cat=100&_nc_sid=ca434c&_nc_eui2=AeGGYE9ZP92ZI3vFVIvliZYnqNg8ntm6FNDfAUj105JQcpWxnTsynOxEFBkJrts1JZO9TnuCdfG43Ex-iY6t7ug91QctsS2uxfFyC2XBFouCTA&_nc_oc=AQmJEXonKe2o_I3FIKuZj7UUVjO-J20sZuZV3V9vXAsTmavwSR8hnC_R40-6u4ra8H0&_nc_ht=scontent.fcrk1-1.fna&oh=f329404be9f10aff9e397646ca0c48cf&oe=5E949C7B";
+    img_url = "https://scontent.fcrk1-1.fna.fbcdn.net/v/t1.0-9/60627711_2460297753981013_8365258745302745088_n.jpg?_nc_cat=100&_nc_sid=825194&_nc_oc=AQmzbosD8OB2FuTi2rAdOuXhakbQzgn9HDVfwQTfHvml7f7lTiJ93GO-vgcWgbIOkx8&_nc_ht=scontent.fcrk1-1.fna&oh=459f2708e21f6aa45cc50ebe25b4e863&oe=5EC01DFB";
     $('.card-image').attr("style", `background-image: url('${img_url}');`).addClass('cover-photo');
 
     $('div.cl').each(function(i, v) {
