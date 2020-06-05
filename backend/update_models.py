cb0adcae1b4aa33bf09c6b8e2c139d25a85aef25
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-06-05 13:48:40 +0800

    redump local models

--- backend/update_models.py
+++ backend/update_models.py
@@ -22,12 +22,9 @@ def main():
         if len(data) == 0:
             return 0
     for d in data:
-        try:
-            obj, created = Member.objects.update_or_create(id=data[d]['pk'], defaults=dict(**data[d]['fields']))
-            status = 'Created' if created else 'Updated'
-            print(f'{status} entry for {obj.first_name} {obj.last_name}')
-        except:
-            pass
+        obj, created = Member.objects.update_or_create(id=data[d]['pk'], defaults=dict(**data[d]['fields']))
+        status = 'Created' if created else 'Updated'
+        print(f'{status} entry for {obj.first_name} {obj.last_name}')
 
 
 if __name__ == '__main__':
--- release-tasks.sh
+++ release-tasks.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 python manage.py migrate
-python manage.py migrate backend
 python ./backend/update_models.py
